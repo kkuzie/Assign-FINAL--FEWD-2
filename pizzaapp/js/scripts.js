332f08339fb6037a7c4bc91fd192a45a0719ee59
kkuzie <kkuzii@yahoo.it>
2020-05-11 20:05:26 -0700

    part1c regex/val completed for gottafindya

--- pizzaapp/js/scripts.js
+++ pizzaapp/js/scripts.js
@@ -112,7 +112,7 @@ $('#dwelling2').change (function(){
   }else {
     $('#other2').hide();
   }
-});
+});//closer for #dwelling2.change func
 
 //GOTTAFINDYA & STREETCREDS FORM VALIDATION
 
@@ -129,6 +129,7 @@ let email = document.getElementById('email');
 let fone = document.getElementById('fone');
 
 //STREET CREDS
+//gets the input tag for each label of form - name, address, etc):
 let name2 = document.getElementById('whosbuying2');
 // let address2 = document.getElementById('address2');
 // let apt2 = document.getElementById('apt-ste-num2');
@@ -142,33 +143,75 @@ let fone2 = document.getElementById('fone2');
 //SUBMIT BUTTON for gottafindya and streetcreds
 let payup = document.getElementById('payup-btn');
 
-function checkName(){
-  let regexVal = {
-    name: /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{1,}+ [a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{1,}+$/,
-    city:/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{1,}+$/,
-    state: /^(?:(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]))$/,
-    zip: /^\d{5}$|^\d{5}-\d{4}$/, 
-    phone: /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/,
-    email: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+//GOTTAFINDYA
+//grabs #gottafindya form:
+let gottafindya = document.forms.gottafindya;
+console.log(gottafindya);
+
+//grabs all input tags within #gottafindya
+let gottafindyaInputs = document.querySelectorAll('#gottafindya input:not(.notincl)');
+console.log(gottafindyaInputs);
+
+//REGEX patterns for each input field:
+let patterns = {//^ is for start and $ is for the end - the stuff inbetween has to start and end with that part of regex (so if letters incl will be invalid)
+      name: /^(?![\s.]+$)[a-zA-Z\s.]*$/,
+      // address: /^\d+[ ](?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Lane|Road|Boulevard|Drive|Street|Ave|Dr|Rd|Blvd|Ln|St)\.?/,
+      citytype:/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/,
+      statetype:/^([Aa][LKSZRAEPlkszraep]|[Cc][AOTaot]|[Dd][ECec]|[Ff][LMlm]|[Gg][AUau]|[Hh][Ii]|[Ii][ADLNadln]|[Kk][SYsy]|[Ll][Aa]|[Mm][ADEHINOPSTadehinopst]|[Nn][CDEHJMVYcdehjmvy]|[Oo][HKRhkr]|[Pp][ARWarw]|[Rr][Ii]|[Ss][CDcd]|[Tt][NXnx]|[Uu][Tt]|[Vv][AITait]|[Ww][AIVYaivy])$/,
+      zip: /(^\d{5}$)|(^\d{9}$)|(^\d{5}-\d{4}$)/,
+      phone:  /^\D?([2-9]{1})(\d{2})\D?\D?(\d{3})\D?(\d{4})$/
+
+}
+
+//VALIDATION FUNCTION
+function validate(field, regex){//1st param is the name field testing 
+  //2nd param is the regex we want to test against
+  console.log(regex.test(field.value));//find out IF something is valid - the way to test against a regex pattern is regex.test and the value we want to test - the field
+  //so when someone types into a particular field, we grab the the value (field.value)and send it into the validate function.
+  //also going to grab one of the regex patterns and setting it into the regex parameter
+
+  //then going to take the regex parameter and test it (regex.test) and pass in the thing we want to test it against (field.value)
+
+  //this will return true or false whether the input field is valid
+  
+  if(regex.test(field.value)){
+    field.className = 'form-control valid';
+    // field.name.style.border = '3px solid midnightblue'; 
+    $('.errorFone').text('');
+  } else {
+    field.className = 'form-control invalid';
+    // field.name.style.border = '3px solid crimson';//grabs gottafindya var (form) then the name value change the style border
+    $('.errorFone').text("11 digits only, please");
+    // $('.foneError').append('<p>11 digits only, please');
+  }
+}//closing for function validate()
 
 
-  //SCANNERS
-  let nameScanner = nameRegCheck.test(name.value);
-  // let emailScanner = emailRegCheck.test(email.value);
+gottafindyaInputs.forEach((input) => {
+    input.addEventListener('keyup', e => {
+        console.log(e.target.attributes.name.value);//take event, target the element that the event occurred on, get the attribute of target, then name attribute, then value of name attr.
 
+        //call validate function:
+        validate(e.target, patterns[e.target.attributes.name.value]);//1st param is the input field (e.target)
+        //2nd param is the regex pattern - patterns is the object name, 
+        //then in [] will get value of whatever the value of the name property on each of the input fields when type in it
+        //which then pass that into the patterns object and find that regular expression
 
+        //essentially, passing the target element and regular expression for that element.
 
+    });//closer for input.addEventListener-keyup
+    gottafindya.reset();
 
-}//closer for function checkName
-
-
-
-
-
+});//closer for gottafindyaInputs.forEach()
 
 
 
 });//closing document ready
+
+
+  //SCANNERS
+  // let nameScanner = nameRegCheck.test(name.value);
+  // let emailScanner = emailRegCheck.test(email.value);
 
 
 //***FIRST TRY NAME VALIDATION START
@@ -203,6 +246,74 @@ function checkName(){
 // checkName();
 //FIRST TRY NAME VALIDATION END*** 
 
+//***SECOND TRY NAME VALIDATION START
+//TypeError: regexVal[$(...).attr(...)] is undefined
+// $(function (){//regex function
+  //   let regexVal = {
+  //     name: /^(?![\s.]+$)[a-zA-Z\s.]*$/
+  //     // city:/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{1,}+$/,
+  //     // state:/^([Aa][LKSZRAEPlkszraep]|[Cc][AOTaot]|[Dd][ECec]|[Ff][LMlm]|[Gg][AUau]|[Hh][Ii]|[Ii][ADLNadln]|[Kk][SYsy]|[Ll][Aa]|[Mm][ADEHINOPSTadehinopst]|[Nn][CDEHJMVYcdehjmvy]|[Oo][HKRhkr]|[Pp][ARWarw]|[Rr][Ii]|[Ss][CDcd]|[Tt][NXnx]|[Uu][Tt]|[Vv][AITait]|[Ww][AIVYaivy])$/,
+  //     // zip: /^\d{5}$|^\d{5}-\d{4}$/, 
+  //     // phone: /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/,
+  //     // email: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+  //   };
+  
+  // $.each($('#gottafindya input:not(".notincl")'), function(){//select every input inside gottafindya form except add,apt,dwelllingtype
+  // //then callback function
+  
+  // //runs and checks input value with regex rules:
+  //   $(this).on('focusout', function(){//on focusout from input, callback function
+  //     //checks if input value = regex rules
+  //     //if yes, pass
+  //     //if no, add error class from CSS file?????
+  //       if(!regexVal[$(this).attr('name')].test($(this).val())){//if regex is not correct-use object and JSON obj???
+  //         //'this' refers to input in #gottafindya input:not(".notincl") that has attribute 'name' that will test the value
+  //             $(this).addClass('error');//now add class 'error'
+  //       }//closer for ifstmt 
+  //       else {//otherwise remove class 'error'
+  //             $(this).removeClass('error');
+  //       }//closer for else clause
+  //   });//closer for .on('focusout'...
+  // });//closer for $.each
+  // });//closer for regex function 
+//SECOND TRY NAME VALIDATION END***  
+  
+//***THIRD TRY REGEX VALIDATION START
+// TypeError: name.test is not a function
+// function valid (){
+//   let regexVal = {
+//         name: /^(?![\s.]+$)[a-zA-Z\s.]*$/}
+        
+//         if (name.test(name)){
+//   document.getElementById('nameError').innerHTML() = ' ';
+// } else {
+//   document.getElementById('nameError').innerHTML() = 'invalid';
+//   return false;
+// }
+// }
+// valid();
+//THIRD TRY REGEX VALIDATION END
+
+//***USED WITH FOURTH TRY REGEX VALIDATION START
+// let regexVal = /^(?![\s.]+$)[a-zA-Z\s.]*$/;
+// console.log(regexVal);
+
+// $('#payup-btn').on('focus', e=> {//WHY DOES NOT WORK WITH SUBMIT????
+  //??????????
+  // e.preventDefault();
+// if (name.value.trim() == '' || name.value.trim() !== regexVal) {
+//   gottafindya.name.style.border = '3px solid crimson';//grabs gottafindya var (form) then the name value change the style border
+//   $('#nameError').text('only letters please');
+// }
+// });
+
+//ON FOCUS changes border on GOTTAFINDYA AND STREET CREDS input boxes
+// gottafindya.name.onfocus = function(){
+//   gottafindya.name.style.border = '3px solid midnightblue'; 
+// };
+
+
+//USED WITH FOURTH TRY REGEX VALIDATION END***
 
 //   $("#pickup-btn").click(function() {
 //     $("#order-content").show();
